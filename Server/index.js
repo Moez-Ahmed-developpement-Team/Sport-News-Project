3deafb02608f7e7dbc54ffe2d1a12843e9c679ad
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-09-14 17:58:19 +0100

    routes

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,17 @@ const express = require("express");
 const app = express();
 
 const PORT =  3069 ;
+///
+const adminRoutes = require ('./Routes/AdminRoutes') ;
+const usersRoutes = require ('./Routes/UsersRoutes') ;
+const viewersRoutes = require ('./Routes/ViewersRoutes') ; 
+
+app.use("/admin",adminRoutes) ;
+app.use("/user",usersRoutes) ;
+app.use("/view",viewersRoutes) ;
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
