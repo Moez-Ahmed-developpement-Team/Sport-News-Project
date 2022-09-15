c4ad5eb6afa0b2aec48742f48625efd456e845d9
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-09-15 18:19:46 +0100

    test

---
+++ client/src/components/AddTest.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="test">
+        <button @click="postImage()">Test Moez</button>
+    </div>
+</template>
+
+<script>
+    import picture from "../assets/logo.png"
+    import axios from "axios"
+export default {
+    
+    methods: {
+
+
+        postImage : async () => {
+            try {
+                const response = await axios.post(
+                    "https://api.cloudinary.com/v1_1/dtwuychif/image/upload",
+                    picture
+                )
+                console.log(response.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
+}
+</script>
