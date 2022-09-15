665fbb76ba3767d788d32b972d5df5aef94ff3bd
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-09-15 12:55:09 +0100

    making vue app for admin and clent tested view without any modification yet

---
+++ client/src/components/All-posts.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div id="News" class="AllNews">
+    <h1> All the News </h1>
+    <div class="box">{{Allposts}}</div>
+    <li>aaa</li>
+    <li>bbb</li>
+    <li>ccc</li>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+export default {
+  data() {
+    return {
+      Allposts: ['ahmed Sassi']
+    }
+  },
+
+methods: {
+  getAll(){axios.get('http://localhost:3069/').then((result)=>console.log(result)).catch((err)=>console.log(err))
+    }
+},
+}
+// const getAll = axios.get('http://localhost:3069/').then((result)=>console.log(result)).catch((err)=>console.log(err))
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
