56293157e8c816d0af603da530f315f06fd6b4b3
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-09-16 08:15:44 +0100

    morning actualisation

--- client/src/components/AddTest.vue
+++ client/src/components/AddTest.vue
@@ -5,24 +5,5 @@
 </template>
 
 <script>
-    import picture from "../assets/logo.png"
-    import axios from "axios"
-export default {
-    
-    methods: {
 
-
-        postImage : async () => {
-            try {
-                const response = await axios.post(
-                    "https://api.cloudinary.com/v1_1/dtwuychif/image/upload",
-                    picture
-                )
-                console.log(response.data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-    }
-}
 </script>
