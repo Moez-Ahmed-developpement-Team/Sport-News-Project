ee1e73424a51cdf8c9756cfe587b36f0b3e986a5
Moez-Temimi <moez.temimi@yahoo.com>
2022-09-16 19:40:09 +0200

    update the work with my teammates

--- client/src/components/AddTest.vue
+++ client/src/components/AddTest.vue
@@ -1,9 +1,39 @@
 <template>
     <div id="test">
-        <button @click="postImage()">Test Moez</button>
+        <input type="file" id="avatar" name="avatar" accept="image/*" v-on:change="changeFile($event)">
+        <button @click="postImage()">upload</button>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+export default {
+    data() {
+        return {
+            img: ""
+        };
+    },
+    
+    methods: {
+         
+    changeFile (e) {
+        this.img = e.target.files;
+        
+    },
+    postImage() {
+            let data = new FormData();
+                data.append("file",this.img)
+                console.log (data)
+                axios.post(  "cloudinary://156134314836426:oLF8kqJFdyjII-YZzpE0bydWp-g@dtwuychif", data, {
+    headers: { "Content-Type": "multipart/form-data" },
+    
+  })
+  .then((res)=> console.log(res))
+  .catch((err)=> console.error(err))
 
+
+    },
+},
+
+}
 </script>
