07a185f77f91c1d0bc3c56c3da2b404745b0678b
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-09-18 10:31:22 +0100

    authentification and Token set in back cokkies done , vue components for login

--- Server/index.js
+++ Server/index.js
@@ -4,10 +4,9 @@ const commentRoutes = require ('./Routes/Comments.Routes') ;
 const postsRoute = require('./Routes/Posts.routes'); 
 
 const express = require("express");
-// const cors = require ('cors')
+const cors = require ('cors')
 const app = express();
-
-///
+app.use(express.json())
 
 //Add Routes to the middleware handling path, specifying the respective URL path
 app.use("/",adminRoutes) ;
@@ -18,12 +17,12 @@ app.use("/",postsRoute) ;
 
 
 const PORT =  3000 ;
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
